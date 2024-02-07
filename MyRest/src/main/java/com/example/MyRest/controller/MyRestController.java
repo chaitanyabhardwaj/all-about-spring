be828dc84e7e2c5005c5382cf47d25a4fffd39f1
Chaitanya Bhardwaj <chaitanyabhardwaj89@gmail.com>
2024-02-07 18:00:11 +0530

    Actuator and dev-tools

--- MyRest/src/main/java/com/example/MyRest/controller/MyRestController.java
+++ MyRest/src/main/java/com/example/MyRest/controller/MyRestController.java
@@ -1,2 +1,35 @@
-package com.example.MyRest.controller;public class MyRestController {
+package com.example.MyRest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MyRestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "It's working!";
+    }
+
+    @GetMapping("/animalFarm")
+    public String animalFarm(
+            @RequestParam(name = "animal1") String animal1,
+            @RequestParam(name = "animal2") String animal2) {
+        String a1 = animal1;
+        String a2 = animal1;
+        if(animal1.length() < animal2.length()) a1 = animal2;
+        else a2 = animal2;
+        return "A %s jumps over a %s".formatted(a1,a2);
+    }
+
+    @GetMapping("/suggestAnimal")
+    public String suggestAnimal() {
+        List<String> animalList = List.of("Dog", "Cat", "Bird", "Tiger", "Elephant", "Camel", "Donkey", "Lizard");
+        int i = ((int)Math.floor(Math.random() * 10)) % 8;
+        return animalList.get(i);
+    }
+
 }
