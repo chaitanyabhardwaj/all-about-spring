202ad768f7f4769e737194a057f23c9c8156bc7a
Chaitanya Bhardwaj <chaitanyabhardwaj89@gmail.com>
2024-03-05 13:42:32 +0530

    Added mysql and JPA support

--- MyRest/create_user.sql
+++ MyRest/create_user.sql
@@ -2,4 +2,4 @@ DROP USER IF EXISTS 'myrestappuser'@'localhost';
 
 CREATE USER 'myrestappuser'@'localhost' IDENTIFIED BY 'pass12345';
 
-GRANT ALL PRIVILEGES ON myrestappdb TO 'myrestappuser'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'myrestappuser'@'localhost';
--- MyRest/src/main/java/com/example/MyRest/MyRestApplication.java
+++ MyRest/src/main/java/com/example/MyRest/MyRestApplication.java
@@ -2,8 +2,10 @@ package com.example.MyRest;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
 		scanBasePackages = {"com.example"})
@@ -21,6 +23,13 @@ public class MyRestApplication {
 	@PreDestroy
 	public void onDestroy() {
 		System.out.println("Bean " + getClass().getSimpleName() + " is being destroyed!");
+	}
+
+	@Bean
+	public CommandLineRunner commandRun(String[] args) {
+		return runner -> {
+			System.out.println("This is a command line runner!");
+		};
 	}
 
 }
